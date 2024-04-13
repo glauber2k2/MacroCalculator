5180e5067e0dd0a2a1c4e471182fc23b679d852d
DevGlauber <devglaubermonteiro@gmail.com>
2024-04-13 01:30:41 -0300

    aumentado margin do copyright

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function page() {
   return (
     <div className="md:p-10 flex justify-center items-center h-full flex-col">
       <MacroCalculator />
-      <p className="mt-2 text-xs opacity-80">
+      <p className="mt-4 text-xs opacity-80">
         &copy; Desenvolvido por{' '}
         <Link href={'https://glauber.vercel.app/'} className="underline">
           DevGlauber.
